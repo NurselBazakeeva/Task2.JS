ae055f33e92b0e676febd83b524e621df659c283
Nursel Bazakeeva <nursel.bazakeeva.kk@gmail.com>
2022-01-29 03:28:50 +0600

    added task 2

---
+++ script2.js
@@ -0,0 +1,5 @@
+// Дана строка 'js'. Сделайте из нее строку 'JS'.
+
+let str = 'js';
+let newStr = str.toUpperCase();
+console.log(newStr);
---
+++ script4.js
@@ -0,0 +1,6 @@
+/* Дана строка'I am a hero!'. 
+Выведите в консоль количество символов в этой строке.*/
+
+let str = "I am a hero!";
+let numberOfSymbols = str.length;
+console.log(numberOfSymbols);
